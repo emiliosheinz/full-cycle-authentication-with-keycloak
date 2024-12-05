c6db4d595b02817b28f7660f44a2033bf6a4b0ee
Emilio Heinzmann <emiliosheinz@gmail.com>
2024-12-04 21:31:54 -0300

    feat: implement nonce

--- authentication-flow/authorization-code-flow/src/index.ts
+++ authentication-flow/authorization-code-flow/src/index.ts
@@ -1,41 +1,136 @@
-import express from 'express';
+import express from "express";
+import session from "express-session";
+import jwt from "jsonwebtoken";
+import crypto from "crypto";
 
 const app = express();
 
-app.get('/login', (_, res) => {
-  const loginParams = new URLSearchParams({
-    client_id: 'fullcycle-client',
-    redirect_uri: 'http://localhost:3000/callback',
-    response_type: 'code',
-    scope: 'openid'
+const memoryStore = new session.MemoryStore();
+
+app.use(
+  session({
+    secret: "my-secret",
+    resave: false,
+    saveUninitialized: false,
+    store: memoryStore,
   })
+);
+
+const middlewareIsAuth = (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  //@ts-expect-error - type mismatch
+  if (!req.session.user) {
+    return res.redirect("/login");
+  }
+  next();
+};
+
+app.get("/login", (req, res) => {
+  const nonce = crypto.randomBytes(16).toString("base64");
+  const state = crypto.randomBytes(16).toString("base64");
+
+  //@ts-expect-error - type mismatch
+  req.session.nonce = nonce;
+  //@ts-expect-error - type mismatch
+  req.session.state = state;
+  req.session.save();
+
+  const loginParams = new URLSearchParams({
+    client_id: "fullcycle-client",
+    redirect_uri: "http://localhost:3000/callback",
+    response_type: "code",
+    scope: "openid",
+    nonce,
+    state
+  });
 
   const url = `http://localhost:8080/realms/fullcycle-realm/protocol/openid-connect/auth?${loginParams.toString()}`;
+  
+  res.redirect(url);
+});
 
-  res.redirect(url)
-}); 
+app.get("/logout", (req, res) => {
+  const logoutParams = new URLSearchParams({
+    //@ts-expect-error
+    id_token_hint: req.session.id_token,
+    post_logout_redirect_uri: "http://localhost:3000/login",
+  });
 
-app.get('/callback',  async (req, res) => {
-  const params = new URLSearchParams({
-    client_id: 'fullcycle-client',
-    grant_type: 'authorization_code',
+  req.session.destroy((err) => {
+    console.error(err);
+  });
+
+  const url = `http://localhost:8080/realms/fullcycle-realm/protocol/openid-connect/logout?${logoutParams.toString()}`;
+  res.redirect(url);
+});
+
+app.get("/callback", async (req, res) => {
+  //@ts-expect-error - type mismatch
+  if (req.session.user) {
+    res.redirect("/admin");
+    return;
+  }
+
+  //@ts-expect-error - type mismatch
+  if(req.query.state !== req.session.state) {
+    res.status(401).json({ message: "Unauthorized" }); 
+    return
+  }
+
+  const bodyParams = new URLSearchParams({
+    client_id: "fullcycle-client",
+    grant_type: "authorization_code",
     code: req.query.code as string,
-    redirect_uri: 'http://localhost:3000/callback'
-  })
+    redirect_uri: "http://localhost:3000/callback",
+  });
 
-  const url = "http://keycloak:8080/realms/fullcycle-realm/protocol/openid-connect/token"
+  const url = `http://keycloak:8080/realms/fullcycle-realm/protocol/openid-connect/token`;
 
- const  response = await fetch(url, {
-    method: 'POST',
+  const response = await fetch(url, {
+    method: "POST",
     headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
+      "Content-Type": "application/x-www-form-urlencoded",
     },
-    body: params.toString()
-  })
-  const result = await response.json()
-  res.status(200).send(result)
-})
+    body: bodyParams.toString(),
+  });
+
+  const result = await response.json();
+
+  const payloadAccessToken = jwt.decode(result.access_token) as any;
+  const payloadRefreshToken = jwt.decode(result.refresh_token) as any;
+  const payloadIdToken = jwt.decode(result.id_token) as any;
+
+  if (
+    //@ts-expect-error - type mismatch
+    payloadAccessToken.nonce !== req.session.nonce ||
+    //@ts-expect-error - type mismatch
+    payloadRefreshToken.nonce !== req.session.nonce ||
+    //@ts-expect-error - type mismatch
+    payloadIdToken.nonce !== req.session.nonce
+  ) {
+    res.status(401).json({ message: "Unauthorized" });
+    return;
+  }
+
+  console.log(payloadAccessToken);
+  //@ts-expect-error - type mismatch
+  req.session.user = payloadAccessToken;
+  //@ts-expect-error - type mismatch
+  req.session.access_token = result.access_token;
+  //@ts-expect-error - type mismatch
+  req.session.id_token = result.id_token;
+  req.session.save();
+  res.json(result);
+});
+
+app.get("/admin", middlewareIsAuth, (req, res) => {
+  //@ts-expect-error - type mismatch
+  res.json(req.session.user);
+});
 
 app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-})
+  console.log("Server is running on port 3000");
+});
